554569202b099fda6a49de8ba81ebc89675a8455
Marcelo Oliveira <marcelo@localhost.localdomain>
2017-03-08 22:39:15 -0300

    - front-end

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask import abort
 from flask import make_response
 from flask import request
 from flask import url_for
+from flaskext.mysql import MySQL
 import requests, json
 
 app = Flask(__name__)
 
-@app.route('/cep/<string:task_id>', methods=['GET'])
-def get_task(task_id):
-    r = requests.get('http://api.postmon.com.br/v1/cep/' + task_id)
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'cep'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+@app.route('/cep')
+def showMain():
+    return render_template('index.html')
+
+@app.route('/cep', methods=['POST'])
+def cep():
+    _cep = request.form['cep']
+    r = requests.get('http://api.postmon.com.br/v1/cep/' + _cep)
     return app.response_class(r.content, content_type='application/json')
 
 @app.errorhandler(404)
